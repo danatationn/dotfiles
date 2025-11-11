07d3f2294c5b94bc6d6f4c33246a7b5f78b36571
dan <lemontasteicetea@proton.me>
2025-11-11 15:10:45 +0200

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,31 +1,29 @@
 if status is-interactive
 
 # aliases
-alias v 'nvim'
-alias r 'ranger'
-alias p 'paru'
-alias pi 'paru -Syu'
-alias pr 'paru -Rcns'	
-alias ff 'fastfetch'
 alias mv 'mv -i'
 alias cp 'cp -i'
 alias ln 'ln -i'
 alias rm 'rm -I'
-alias c clear
+alias killjobs 'disown (jobs -p)'
 
 # misc
-set -gx CPM_SOURCE_CACHE "$HOME/.cache/CPM/"
+set -gx GEODE_SDK $HOME/.local/share/Geode/sdk
+set -gx CPM_SOURCE_CACHE $HOME/.cache/CPM/
 fish_add_path $HOME/.millennium/ext/bin/
-fish_add_path $HOME/.local/share/coursier/bin/
-export SUDO_PROMPT="$(tput setaf 11) ? $(tput sgr0)"
+fish_add_path $HOME/.local/bin  # why isn't this default 
+export SUDO_PROMPT="$(tput setaf 3) ? $(tput sgr0)"
 zoxide init fish --cmd cd | source
 
 # theme things
+set fish_greeting
+
+## kawasaki
 set -g theme_display_time yes
 set -g theme_display_group no
 set -g theme_display_hostname no
 set -g theme_display_virtualenv no
-set -g theme_color_superuser purple
+set -g theme_color_superuser red
 set -g theme_color_user purple
 set -g theme_prompt_char_normal ' >'
 set -g theme_prompt_char_superuser '>>'
